1d41f34712c7fae53ac7f823ab12e5d1b13afc0e
coulibaly <coulibaly@git.com>
2023-03-02 17:23:13 +0100

    first commi

---
+++ src/DocBlog/views.py
@@ -0,0 +1,10 @@
+
+from django.shortcuts import render
+from datetime import datetime
+
+
+def index(request):
+
+  
+
+    return render(request, "DocBlog/index.html", context={"date": datetime.today()})
